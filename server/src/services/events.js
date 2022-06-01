be53e7230d422b75faf4c9ad8317312949e9a994
Marcus Lindvall <marcus.lindvall@gmail.com>
2022-06-01 12:05:04 +0000

    Revert: Save and restore reference eventID for restarts.

--- server/src/services/events.js
+++ server/src/services/events.js
@@ -1,14 +1,10 @@
-const { getCollection } = require("../ports/database");
 const albion = require("../ports/albion");
 const { publish, subscribe } = require("../ports/queue");
 const logger = require("../helpers/logger");
-const database = require("../database");
 const { sleep } = require("../helpers/utils");
 
 const EVENTS_EXCHANGE = "events";
 const EVENTS_QUEUE_PREFIX = "events";
-
-const EVENT_COLLECTION = "eventInfo";
 
 async function fetchEventsTo(latestEvent, { offset = 0 } = {}, events = []) {
   // Maximum offset reached, just return what we have
@@ -17,41 +13,12 @@ async function fetchEventsTo(latestEvent, { offset = 0 } = {}, events = []) {
     return events;
   }
 
-  const collection = getCollection(EVENT_COLLECTION);
-  if (!collection) throw new Error("Not connected to the database.");
-
   try {
     // If not latestEvent, just fetch a single one to create a reference
     if (!latestEvent) {
-      latestEvent = await collection.findOne()
-
-      if (!latestEvent) latestEvent = { EventId: 0 };
-      if (latestEvent && latestEvent.EventId) {
-        logger.info(`Stored latest event found. Retrieving up to event ${latestEvent.EventId}.`);
-        return await latestEvent;
-      } else {
-        logger.info(`No latest event found. Retrieving first events.`);
-      }
       return await albion.getEvents({
         limit: 1,
       });
-    } else {
-      logger.info(`Fetching Albion Online events from API up to event ${latestEvent.EventId}.`);
-      try {
-        await collection.updateOne(
-          {},
-          {
-            $set: {
-              EventId: latestEvent.EventId,
-            },
-          },
-          {
-            upsert: true,
-          },
-        );
-      } catch (e) {
-        return logger.error(`Failed to update last event ID "${latestEvent.EventId}". (${e})"`);
-      }
     }
 
     logger.verbose(`Fetching events with offset: ${offset}`);
